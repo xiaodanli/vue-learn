8640c8ab6f6e5f5f2f3c3b45d5d758388954493c
lixd <dandan580826@163.com>
2019-02-06 10:29:45 +0800

    字符串新增的方法

---
+++ es6-1/src/js/字符串.js
@@ -0,0 +1,27 @@
+//字符串去空格 
+/*
+    trim()      去掉左右的空格
+    trimLeft()  去掉左边的空格
+    trimRight() 去掉右边的空格
+
+    模板字符串  `${变量}`
+
+    str.repeat(n)    输出重复n次的字符串   n:Number
+
+    str.includes('child')   检测str的内部是否存在child 返回值为布尔值
+
+    str.startsWith('child') 检测str是否以child开始的   返回值为布尔值 
+    
+    str.endsWith('child')   检测str是否以child结尾的   返回值为布尔值
+
+    区别indexOf()   返回的是指定下标，不是布尔值
+
+    str.padStart(n,'@')    在str字符串的前面填充n个@符号  
+    
+    第一个参数：:填充的长度  第二个参数：填充的内容  
+
+    str.padEnd(n,'@')      在str字符串的结尾填充n个@符号
+
+    第一个参数：:填充的长度  第二个参数：填充的内容  
+
+*/
